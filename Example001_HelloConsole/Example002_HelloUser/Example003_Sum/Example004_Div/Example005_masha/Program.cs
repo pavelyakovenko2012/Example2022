cddea250d950eda4cc59b52128c535d38edf9ebf
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-07-08 15:44:36 +0500

    Example005_masha

--- Example001_HelloConsole/Example002_HelloUser/Example003_Sum/Example004_Div/Example005_masha/Program.cs
+++ Example001_HelloConsole/Example002_HelloUser/Example003_Sum/Example004_Div/Example005_masha/Program.cs
@@ -5,8 +5,8 @@ if (username.ToLower() == "маша");
 {
     Console.WriteLine("Ура, это же МАША!");
 }
-    else
-    {
-        Console.Write("Привет, ");
-        Console.WriteLine(username);
-    }
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
